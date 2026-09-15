51ee7dfeaf63b21efc64077d6841a565e50d9972
yuliax000 <ArthyerX@gmail.com>
2026-09-15 15:59:21 +1000

    adjust drum
    adjust drum sound

--- main.js
+++ main.js
@@ -35,7 +35,19 @@ const synth = new Tone.PolySynth(Tone.Synth, {
 }).connect(reverb);
 // const synth = new Tone.Synth().toDestination();
 // create drum synth
-const drumSynth = new Tone.MembraneSynth().toDestination();
+const drumSynth = new Tone.MembraneSynth({
+    pitchDecay: 0.03,
+    octaves: 4,
+    oscillator: {
+        type: "sine"
+    },
+    envelope: {
+        attack: 0.001,
+        decay: 0.25,
+        sustain: 0,
+        release: 0.1
+    }
+}).toDestination();
 
 
 
